d22b4758ece7fad1c5fbdb3093f8cba2446527a8
sukrutwagh <sukrut.wagh@here.com>
2019-05-08 19:14:46 +0530

    AWS Assignment - ALB backed 2 node web servers - Httpd and IIS

---
+++ cf/readme.md
@@ -0,0 +1,6 @@
+# ALB Url
+http://assignment1alb-1835797674.ap-southeast-1.elb.amazonaws.com/
+
+# CDN-S3
+https://d3t2bsdxaw7gl8.cloudfront.net
+https://d3t2bsdxaw7gl8.cloudfront.net/windows/screen-shot1.png
---
+++ windows/ASPwebIIS.ps1
@@ -0,0 +1,16 @@
+﻿Set-ExecutionPolicy Unrestricted
+Set-MpPreference -DisableRealtimeMonitoring $true
+Add-windowsfeature web-webserver -includeallsubfeature -logpath $env:temp\\webserver_addrole.log
+Add-windowsfeature web-mgmt-tools -includeallsubfeature -logpath $env:temp\\mgmttools_addrole.log
+Import-Module WebAdministration
+New-WebSite -Name myapp -Port 80 -HostHeader localhost -PhysicalPath "D:\inetpub\myapp" -Force
+New-WebBinding -Name myapp -IPAddress "*" -Port 80 -HostHeader "*"
+Try {
+    ErrorAction "Stop"
+    Remove-website -Name "Default Web Site" 
+  } 
+ Catch {
+     Write-Output "Unable to remove Default Web Site"
+ }
+start-website -name myapp
+Set-MpPreference -DisableRealtimeMonitoring $false
